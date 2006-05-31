1c6567e4c5c216ac55988b501caa760c1628012a
horseman <horseman@84b5c1e1-3708-0410-9f49-82783c9852aa>
2006-05-31 13:14:54 +0000

    main.h geändert

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -16,6 +16,53 @@
 #define NEWLINE 10
 #define HOMEPAGE_URL "http://ooview.berlios.de"
 
+char *file_choices[] = {
+	"Open",
+	"Close",
+	"Export/Save as...",
+	"Reload",
+	"Print",
+	"Exit"
+};
+
+char *view_choices[] = {
+	"Find",
+	"Find next",
+	"Find previous",
+	"Document info",
+};
+
+char *opts_choices[] = {
+	"External Programs",
+	"Language",
+	"Keybindings",
+};
+
+char *help_choices[] = {
+	"OOView homepage",
+	"Documentation",
+	"Copying",
+	"About OOView",
+};
+
+char *configpath="/etc/ooview/config";
+
+char *returnvalues [] = {
+	"success",
+	"odt file not found",
+	"ovd file not found",
+	"could not open odt file",
+	"could not open ovd file",
+	"could not open txt file",
+	"odt file corruption",
+	"ovd file corruption",
+	"system call returns not 0",
+	"could not write log file. ironic",
+	"language file not found",
+	"no external program defined",
+	"unknown error. should not happen :("
+};
+
 ITEM **file_items;
 ITEM **view_items;
 ITEM **opts_items;
@@ -551,7 +598,7 @@ int main (int argc, char **argv)
 												main_win= subwin(stdscr,LINES-2,COLS,1,0);
 												odt_file = open_odt(file);
 												get_file_content(odt_file, file, buffer);
-												get_file_meta(odt_file, file, buffer);
+
 												close(odt_file);
 												cur_char = buffer->content;
 												print_site(buffer->cur_line, buffer->lines);
--- trunk/src/main.h
+++ trunk/src/main.h
@@ -1,57 +1,11 @@
-char *file_choices[] = {
-	"Open",
-	"Close",
-	"Export/Save as...",
-	"Reload",
-	"Print",
-	"Exit"
-};
-
-char *view_choices[] = {
-	"Find",
-	"Find next",
-	"Find previous",
-	"Document info",
-};
-
-char *opts_choices[] = {
-	"External Programs",
-	"Language",
-	"Keybindings",
-};
-
-char *help_choices[] = {
-	"OOView homepage",
-	"Documentation",
-	"Copying",
-	"About OOView",
-};
-
 int n_choices[4];
 
 char *logfilepath;
-char *configpath="/etc/ooview/config";
 
 char *BROWSER;
 char *PRINTER;
 char *GFX_VIEWER;
 char *DEF_LANG;
-
-char *returnvalues [] = {
-	"success",
-	"odt file not found",
-	"ovd file not found",
-	"could not open odt file",
-	"could not open ovd file",
-	"could not open txt file",
-	"odt file corruption",
-	"ovd file corruption",
-	"system call returns not 0",
-	"could not write log file. ironic",
-	"language file not found",
-	"no external program defined",
-	"unknown error. should not happen :("
-};
 
 struct fileinfo
 {
